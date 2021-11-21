efcf5fe423787a34264d31dd1aef2f2babe6bef0
Stern-Ritter <fadeew.a@mail.ru>
2021-11-22 00:40:04 +0300

    create class Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,1 +1,23 @@
-class Section {}
+export default class Section {
+  constructor({ items, renderer }, selector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(selector);
+  }
+
+  _clear() {
+    this._container.innerHTML = '';
+  }
+
+  renderItems() {
+    this._clear();
+
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(item) {
+    this._container.prepend(item);
+  }
+}
