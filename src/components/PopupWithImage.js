2b111047f29d6b7b5207231405e3fa142a00a24a
Stern-Ritter <fadeew.a@mail.ru>
2021-11-25 23:30:47 +0300

    Add PopupWithImage class

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,1 +1,12 @@
+import Popup from "./Popup";
 
+export default class PopupWithImage extends Popup {
+  open(item) {
+    const imagePic = this._element.querySelector(".image__src");
+    const imageText = this._element.querySelector(".image__place");
+    imagePic.src = item.link;
+    imagePic.alt = `Иллюстрация места ${item.name}`;
+    imageText.textContent = item.name;
+    super.open();
+  }
+}
